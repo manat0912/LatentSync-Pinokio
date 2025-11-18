423b9a9d2497f2baf8dc74d88178e6fb5e28e764
6Morpheus6 <wanton-decay@gmx.net>
2025-11-18 19:28:49 +0100

    install onnxruntime
    - move onnxruntime installation to torch.js
    - pin torch 2.7.0
    - install triton-windows 3.3.1.post19, compatible with torch 2.7.0+cu128
    - matching torch / onnxruntime wheels for linux amd

--- torch.js
+++ torch.js
@@ -8,7 +8,9 @@ module.exports = {
             "venv": "{{args && args.venv ? args.venv : null}}",
             "path": "{{args && args.path ? args.path : '.'}}",
             "message": [
-              "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 xformers==0.0.30 --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps"
+              "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? ' xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps",
+              "{{args && args.triton ? 'uv pip install triton-windows==3.3.1.post19' : ''}}",
+              "uv pip install onnxruntime-gpu"
             ]
           }
         },    // windows amd
@@ -18,7 +20,10 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch-directml torchvision numpy==1.26.4"
+        "message": [
+          "uv pip install torch-directml torchvision numpy==1.26.4",
+          "uv pip install onnxruntime-directml"
+        ]
       }
     },
     // windows cpu
@@ -29,7 +34,8 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          "uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 numpy==1.26.4"
+          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cpu",
+          "uv pip install onnxruntime"
         ]
       }
     },
@@ -40,7 +46,10 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0"
+        "message": [
+          "uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu",
+          "uv pip install onnxruntime"
+        ]
       }
     },
     // linux nvidia
@@ -51,7 +60,9 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 xformers==0.0.30 --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps"
+          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? ' xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps",
+          "{{args && args.triton ? 'uv pip install triton' : ''}}",
+          "uv pip install onnxruntime-gpu"
         ]
       }
     },
@@ -62,7 +73,10 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2.4"
+        "message": [
+          "uv pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/rocm6.3",
+          "uv pip install https://repo.radeon.com/rocm/manylinux/rocm-rel-6.3/onnxruntime_rocm-1.19.0-cp310-cp310-linux_x86_64.whl"
+        ]
       }
     },
     // linux cpu
@@ -72,7 +86,10 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cpu"
+        "message": [
+          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cpu",
+          "uv pip install onnxruntime"
+        ]
       }
     }
   ]
